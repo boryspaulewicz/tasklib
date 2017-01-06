7cbaa33a998b9f1e2fba29a84bca6cc8b39c10dd
Borysław Paulewicz <borys.paulewicz@gmail.com>
2017-01-06 23:05:11 +0100

    state_start()

--- src/States.hpp
+++ src/States.hpp
@@ -7,18 +7,19 @@ class States : public Timer{
 
 private:
 
-  time_type state_start;
+  time_type state_start_ms;
   int current_state = 0;
   
 public:
 
   void set_state(int s){
     current_state = s;
-    state_start = time_ms();
+    state_start_ms = time_ms();
   }
 
-  time_type state_time(){ return time_ms() - state_start; }
-
+  time_type state_time(){ return time_ms() - state_start_ms; }
+  time_type state_start(){ return state_start_ms; }
+  
   int state(){ return current_state; }
 
 };
