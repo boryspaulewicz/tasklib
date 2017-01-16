957451194f0364ae13ced4a3cffb35f169ba9035
borys <borys.paulewicz@gmail.com>
2017-01-16 09:12:09 +0100

    Poprawione sprawdzanie Ctrl-Escape a Task.run

--- src/Task.cpp
+++ src/Task.cpp
@@ -271,7 +271,7 @@ void Task::run() {
 
         TRIAL_IS_OVER = false;
         set_state(0);
-        while (!TRIAL_IS_OVER && (keyp(KEYESCAPE) <= task_start) && (keyp(KEYLCONTROL) <= task_start)) {
+        while (!TRIAL_IS_OVER && (!(keyp(KEYESCAPE) > task_start) && (keyp(KEYLCONTROL) > task_start))) {
             trial_code(state());
             process_events();
         }
